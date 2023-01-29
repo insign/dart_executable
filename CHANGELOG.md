134d4d4b7424d7d1d1c6e19ae18c9bd2bc0331ba
Hélio <insign@gmail.com>
2023-01-29 17:42:28 -0300

    dart pub convetions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
+## 1.0.1
+- Dart Pub conventions
 ## 1.0.0
-
 - Initial version.
--- lib/src/executable_base.dart
+++ lib/src/executable_base.dart
@@ -1,11 +1,14 @@
 import 'package:process_run/which.dart';
 
+/// A class for dealing with executables.
 class Executable {
   final String cmd;
   static final _whichResults = <String, String?>{};
 
+  /// Constructs a new `Executable` instance with the given [cmd].
   const Executable(this.cmd);
 
+  /// Asynchronously finds the path to the executable [cmd].
   Future<String?> find() async {
     if (_whichResults.containsKey(cmd)) {
       return _whichResults[cmd];
@@ -15,8 +18,10 @@ class Executable {
     return result;
   }
 
+  /// Asynchronously checks if the executable [cmd] exists.
   Future<bool> exists() async => await find() != null;
 
+  /// Synchronously finds the path to the executable [cmd].
   String? findSync() {
     if (_whichResults.containsKey(cmd)) {
       return _whichResults[cmd];
@@ -26,5 +31,6 @@ class Executable {
     return result;
   }
 
+  /// Synchronously checks if the executable [cmd] exists.
   bool existsSync() => findSync() != null;
 }
