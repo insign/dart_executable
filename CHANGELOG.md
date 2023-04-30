3c7d52a6e430f7b37fc19c8cfc92e48b9759407d
Hélio <insign@gmail.com>
2023-04-29 23:40:11 -0300

    upgrade dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+## v1.1.0
+- upgrade dependencies
 ## v1.0.4
 - nothing new
 ## v1.0.0
--- lib/pubspec.dart
+++ lib/pubspec.dart
@@ -2,10 +2,10 @@
 const name = 'executable';
 const description =
     'The executable package simplifies managing executables in Dart. Finds and checks presence, with cached results for improved performance.';
-const version = '1.0.4';
+const version = '1.1.0';
 const repository = 'https://github.com/insign/dart_executable';
 const homepage = 'https://github.com/insign/dart_executable';
 const environment = '{sdk: >=2.17.0 <3.1.0}';
-const dependencies = '{process_run: ^0.12.5+2}';
+const dependencies = '{process_run: >=0.12.5+2 <0.14.0}';
 const devDependencies = '{lints: ^2.0.0, test: ^1.16.0}';
 const platforms = '{linux: null, macos: null, windows: null}';
