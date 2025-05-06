d9c4d7e4fe3cd5e06373f391f6a834391843e871
Hélio <insign@gmail.com>
2025-05-05 22:21:00 -0300

    pub upgrade after years

--- lib/pubspec.dart
+++ lib/pubspec.dart
@@ -2,10 +2,10 @@
 const name = 'executable';
 const description =
     'The executable package simplifies managing executables in Dart. Finds and checks presence, with cached results for improved performance.';
-const version = '1.2.3';
+const version = '1.3.0';
 const repository = 'https://github.com/insign/dart_executable';
 const homepage = 'https://github.com/insign/dart_executable';
-const environment = '{sdk: >=2.17.0 <3.2.0}';
-const dependencies = '{process_run: >=0.12.5+2 <0.14.0}';
-const devDependencies = '{lints: ^3.0.0, test: ^1.16.0}';
+const environment = '{sdk: >=2.17.0 <3.8.0}';
+const dependencies = '{process_run: ^1.2.4}';
+const devDependencies = '{lints: ^5.1.1, test: ^1.16.0}';
 const platforms = '{linux: null, macos: null, windows: null}';
