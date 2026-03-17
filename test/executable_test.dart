ac6c83c9f02b9a287e6ea9ddfd334e839546b4c3
insign <1113045+insign@users.noreply.github.com>
2026-03-17 03:34:20 +0000

    feat: add run and runSync execution methods to Executable class

--- test/executable_test.dart
+++ test/executable_test.dart
@@ -1,5 +1,7 @@
-import 'package:test/test.dart';
+import 'dart:io';
+
 import 'package:executable/executable.dart';
+import 'package:test/test.dart';
 
 void main() {
   test('Test executable existence', () async {
@@ -13,4 +15,32 @@ void main() {
     final result = await ls.find();
     expect(result, isNotNull);
   });
+
+  test('Test run method executes command', () async {
+    final dart = Executable('dart');
+    final result = await dart.run(['--version']);
+    expect(result.exitCode, 0);
+  });
+
+  test(
+    'Test run method throws ProcessException if executable not found',
+    () async {
+      final notFound = Executable('non_existent_executable_12345');
+      expect(() => notFound.run([]), throwsA(isA<ProcessException>()));
+    },
+  );
+
+  test('Test runSync method executes command', () {
+    final dart = Executable('dart');
+    final result = dart.runSync(['--version']);
+    expect(result.exitCode, 0);
+  });
+
+  test(
+    'Test runSync method throws ProcessException if executable not found',
+    () {
+      final notFound = Executable('non_existent_executable_12345');
+      expect(() => notFound.runSync([]), throwsA(isA<ProcessException>()));
+    },
+  );
 }
